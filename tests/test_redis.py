f10a4314392bef0b4a65d2c59c30ba0a617649a6
Stefan Lehmann <stlm@posteo.de>
2020-05-27 14:55:19 +0200

    Add tests

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -1,9 +1,15 @@
+import time
+
 import pytest
 from rediswrapper import RedisWrapper, RedisItem
 
 
-def test_redis_wrapper():
-    client = RedisWrapper(namespace="birdcam")
+@pytest.fixture
+def client():
+    return RedisWrapper(namespace="rediswrapper")
+
+
+def test_keys(client):
     client.add_item(RedisItem("dilation", 4, int))
     client.add_item(RedisItem("show_threshold", True, bool))
     client.apply_defaults()
@@ -27,3 +33,20 @@ def test_redis_wrapper():
 
     client["show_threshold"] = False
     assert client["show_threshold"] is False
+
+
+def test_publish_subscribe(client):
+    client.subscribe("foo")
+
+    time.sleep(0.1)
+    msg = client.get_message()
+    assert msg["channel"] == b"foo"
+    assert msg["type"] == "subscribe"
+    assert msg["data"] == 1
+
+    client.publish("foo", "bar")
+    time.sleep(0.1)
+    msg = client.get_message()
+    assert msg["channel"] == b"foo"
+    assert msg["type"] == "message"
+    assert msg["data"] == b"bar"
